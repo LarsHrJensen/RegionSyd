6031c934d4ebedaa4c9c86ef69d9fc325c63fa31
Lars Jensen <laje63255@edu.ucl.dk>
2024-09-08 15:33:31 +0200

    Ambulace, datahandler klasser

---
+++ Model/Ambulance.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.Model
+{
+    public class Ambulance
+    {
+        public int Id { get; set; }
+        public string Station {  get; set; }
+        public string Status { get; set; }
+        public List<string> Transport {  get; set; }
+
+        public Ambulance(int id, string station, string status)
+        {
+            Id = id; 
+            Station = station; 
+            Status = status; 
+            Transport = new List<string>();
+        }
+
+        public void AssignTask(Transport assign)
+        {
+            Transport.Add(assign);
+            Console.WriteLine("Opgaven er oprettet");
+        }
+
+        public void CompleteTask(Transport complete)
+        {
+
+        }
+
+        public void RemoveTask(Transport remove)
+        {
+            Transport.Remove(remove);
+            Console.WriteLine("Opgaven er slettet");
+        }
+    } 
+}
---
+++ Model/DataHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.Model
+{
+    public class DataHandler
+    {
+        private string filePath;
+        public DataHandler(string path) 
+        { 
+            filePath = path;
+        }
+        public void LoadFromFile()
+        {
+            
+        }
+
+        public void SaveToFile<T>(List<T> items) 
+        { 
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var item in items)
+                {
+                    if (item is Ambulance ambulance)
+                    {
+                        writer.WriteLine($"{ambulance.Id},{ambulance.Station},{ambulance.Status}");
+                    }
+
+                    else if (item is Transport transport)
+                    {
+                        writer.WriteLine("");
+                    }
+                }
+            }
+
+            Console.WriteLine("Data er gemt");
+        }
+    }
+}

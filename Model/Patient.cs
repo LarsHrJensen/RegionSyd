4b62734d21f69a2fb49ec7e6e06e8d64adde4d8b
Your Name <you@example.com>
2024-09-09 10:20:32 +0200

    Patient

---
+++ Model/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.Model
+{
+    internal class Patient
+    {
+    public char CPR { get; set; }
+    public string FullName { get; set; }
+    public string Status { get; set; }
+
+
+        public Patient(char cpr, string fullName, string status) 
+        {
+         CPR = cpr;
+        FullName = fullName;
+        Status = status;
+        }
+
+
+    }
+}
---
+++ View/AddPatientView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RegionSyd.View
+{
+    /// <summary>
+    /// Interaction logic for AddPatientView.xaml
+    /// </summary>
+    public partial class AddPatientView : Window
+    {
+        public AddPatientView()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}

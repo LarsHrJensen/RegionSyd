cae3dba3879b45ca76e1991cb19f9a99928b5104
Rasmus Wissing Kallehauge <rkallehauge@gmail.com>
2024-09-09 10:18:50 +0200

    changes addtransportview and vm viewmodelbase other changes probably

---
+++ VIewModel/ViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.VIewModel
+{
+    internal class ViewModelBase : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}

dfdda568a5c45c5361017701af37360f48622b5c
Rasmus Wissing Kallehauge <rkallehauge@gmail.com>
2024-09-26 12:18:50 +0200

    trying out graph theory for this project

---
+++ Model/Helper/TravelManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.Model.Helper
+{
+    public class TravelManager
+    {
+        IConfiguration _configuration;
+
+        public TravelManager(IConfiguration config)
+        {
+            _configuration = config;
+        }
+
+        public void Setup()
+        {
+            // check if travelnodes are setup
+
+            // if not setup travelnodes
+        }
+    }
+}
---
+++ Model/TravelNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.Model
+{
+    public class TravelNode
+    {
+        public bool IsHospital { get; }
+        public Hospital? Hospital { get; } = null;
+        public string Name { get; }
+        public int Id { get; }
+
+        private Dictionary<TravelNode, int> _connections;
+
+        public TravelNode(bool isHospital, Hospital? hospital, string name, int id)
+        {
+            IsHospital = isHospital;
+            Hospital = hospital;
+            Name = name;
+            Id = id;
+
+            _connections = new Dictionary<TravelNode, int>();
+        }
+
+        public void AddConnection(TravelNode node, int distance)
+        {
+
+            // If connection already exists, return
+            if (_connections.ContainsKey(node)) return;
+
+            // Set A -> B with distance
+            _connections.Add(node, distance);
+
+            // Set B -> A with distance
+            node.AddConnection(this, distance);
+        }
+
+        public bool Contains(TravelNode node)
+        {
+            return _connections.ContainsKey(node);
+        }
+
+        public Dictionary<TravelNode, int> GetConnections()
+        {
+            return _connections;
+        }
+    }
+}
